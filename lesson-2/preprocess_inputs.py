7a78e666f53ea8c9e0ac5230207144034b672798
guillain bisimwa <guillainbisimwa@hotmail.fr>
2019-12-30 09:13:50 -0500

    preprocessing

---
+++ lesson-2/preprocess_inputs.py
@@ -0,0 +1,56 @@
+import cv2
+import numpy as np
+
+def preprocessing(input_image, height, width):
+    image = cv2.resize(input_image, (width, height))
+    image = image.transpose((2,0,1))
+    image = image.reshape(1, 3, height, width)
+
+    return image
+
+
+def pose_estimation(input_image):
+    '''
+    Given some input image, preprocess the image so that
+    it can be used with the related pose estimation model
+    you downloaded previously. You can use cv2.resize()
+    to resize the image.
+    '''
+    preprocessed_image = np.copy(input_image)
+
+    # TODO: Preprocess the image for the pose estimation model
+    preprocessed_image = preprocessing(preprocessed_image, 256, 456)
+
+    return preprocessed_image
+
+
+def text_detection(input_image):
+    '''
+    Given some input image, preprocess the image so that
+    it can be used with the related text detection model
+    you downloaded previously. You can use cv2.resize()
+    to resize the image.
+    '''
+    preprocessed_image = np.copy(input_image)
+
+    # TODO: Preprocess the image for the text detection model
+    preprocessed_image = preprocessing(preprocessed_image, 768, 1280)
+
+
+    return preprocessed_image
+
+
+def car_meta(input_image):
+    '''
+    Given some input image, preprocess the image so that
+    it can be used with the related car metadata model
+    you downloaded previously. You can use cv2.resize()
+    to resize the image.
+    '''
+    preprocessed_image = np.copy(input_image)
+
+    # TODO: Preprocess the image for the car metadata model
+    preprocessed_image = preprocessing(preprocessed_image, 72, 72)
+
+
+    return preprocessed_image
